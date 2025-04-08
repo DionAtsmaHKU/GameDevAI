cf70ee9ce881e02ee75defef4b7639ce01675bbe
Dion Atsma <dion.atsma@student.hku.nl>
2025-04-08 16:03:02 +0200

    Simple raycast added

--- BehaviourTreeExample/Assets/Scripts/AI/Agents/Guard.cs
+++ BehaviourTreeExample/Assets/Scripts/AI/Agents/Guard.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class Guard : MonoBehaviour
 {
@@ -10,7 +12,13 @@ public class Guard : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private List<Transform> waypoints = new List<Transform>();
     [SerializeField] private Transform weapon;
+    [SerializeField] private Transform head;
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private LayerMask wallLayer;
 
+    [Range(1, 360)] public float angle = 180f;
+
+    private Vector3 offset = new Vector3(0, -10, 0);
     private BTBaseNode tree;
     private NavMeshAgent agent;
     private Animator animator;
@@ -26,7 +34,7 @@ public class Guard : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponentInChildren<Animator>();
         // TestFunc += ReturnFalse;
-        SeesPlayer += GuardSeesPlayer;
+        SeesPlayer += SpotPlayerRaycast;
         HasWeapon += GuardHasWeapon;
         InAttackRange += GuardInAttackRange;
     }
@@ -78,7 +86,8 @@ public class Guard : MonoBehaviour
 
     private void FixedUpdate()
     {
-        SeePlayerCheck();
+        // GuardSeesPlayer();
+        SpotPlayerRaycast();
         TaskStatus result = tree.Tick();
         blackboard.SetVariable(VariableNames.TARGET_POSITION_PLAYER, player.transform.position);
         Debug.Log(blackboard.GetVariable<State>(VariableNames.STATE));
@@ -102,6 +111,7 @@ public class Guard : MonoBehaviour
         return false;
     }
 
+    /*
     // Will use a raycast or the like eventually, currently used for easy testing
     private bool GuardSeesPlayer()
     {
@@ -124,6 +134,51 @@ public class Guard : MonoBehaviour
             blackboard.SetVariable(VariableNames.SEES_PLAYER, true);
         }
         else { blackboard.SetVariable(VariableNames.SEES_PLAYER, false); }
+    }
+    */
+
+    /*
+    private void FOV()
+    {
+        Collider2D[] rangeCheck = Physics2D.OverlapCircleAll(transform.position, 10, playerLayer);
+
+        if (rangeCheck.Length > 0)
+        {
+            Transform target = rangeCheck[0].transform;
+            Vector2 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector2.Angle(transform.up, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+                if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionLayer))
+                    CanSeePlayer = true;
+                else
+                    CanSeePlayer = false;
+            }
+            else
+                CanSeePlayer = false;
+        }
+        else if (CanSeePlayer)
+            CanSeePlayer = false;
+    }
+    */
+
+    private bool SpotPlayerRaycast()
+    {
+        Vector3 directionToTarget = (player.transform.position - head.position).normalized;
+        float distanceToTarget = Vector3.Distance(head.position, player.transform.position);
+        if (Physics.Raycast(head.position, directionToTarget, distanceToTarget, playerLayer))
+        {
+            if (!Physics.Raycast(head.position, directionToTarget, distanceToTarget, wallLayer))
+            {
+                blackboard.SetVariable(VariableNames.SEES_PLAYER, true);
+                return true;
+            }
+        }
+        blackboard.SetVariable(VariableNames.SEES_PLAYER, false);
+        Debug.Log("no player??");
+        return false;
     }
 
     private bool GuardHasWeapon()
